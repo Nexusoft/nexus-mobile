9e97dea0926cf4a095a29878dfff89bf47eff6b6
Krysto <quy.p.hoang@gmail.com>
2020-05-06 15:09:49 +0700

    icons for drawer side menu

--- src/navigation/SideMenu.js
+++ src/navigation/SideMenu.js
@@ -3,12 +3,18 @@ import styled from "@emotion/native";
 
 import { Text } from "components/StyledText";
 import NativeButton from "components/NativeButton";
+import Component from "components/Component";
 import { navigate } from "navigation/container";
+import UserIcon from "icons/user.svg";
+import TokenIcon from "icons/token.svg";
+import NameIcon from "icons/abc.svg";
+import NamespaceIcon from "icons/abc-cube.svg";
+import AssetIcon from "icons/asset.svg";
 
-const StyledSideMenu = styled.View(({ theme }) => ({
+const StyledSideMenu = styled.View({
   flex: 1,
   paddingTop: 56,
-}));
+});
 
 const Divider = styled.View(({ theme }) => ({
   height: 0.33,
@@ -20,93 +26,76 @@ const MenuHeader = styled.View({
   paddingHorizontal: 20,
 });
 
-const UserInfo = styled.View({});
+const UserInfo = styled.View({
+  flexDirection: "row",
+  alignItems: "center",
+});
+
+const UserAvatar = styled(UserIcon)(({ theme }) => ({
+  color: theme.primary,
+  width: 25,
+  height: 25,
+  marginRight: 15,
+}));
 
 const UserName = styled(Text)(({ theme }) => ({
   color: theme.primary,
   fontSize: 24,
 }));
 
-const MenuItem = styled.View(({ theme }) => ({
+const MenuItemWrapper = styled.View({
   paddingVertical: 10,
   paddingHorizontal: 20,
-  color: theme.foreground,
+  flexDirection: "row",
+  alignItems: "center",
+});
+
+const MenuItemIcon = styled(Component)(({ theme }) => ({
+  color: theme.foregroundEmphasis,
+  width: 14,
+  height: 14,
+  marginRight: 10,
 }));
+
+const MenuItem = ({ linkTo, icon, label }) => (
+  <NativeButton
+    borderless={false}
+    onPress={() => {
+      if (linkTo) {
+        navigate(linkTo);
+      }
+    }}
+  >
+    <MenuItemWrapper>
+      {!!icon && <MenuItemIcon as={icon} />}
+      <Text emphasis>{label}</Text>
+    </MenuItemWrapper>
+  </NativeButton>
+);
 
 export default function SideMenu(props) {
   return (
     <StyledSideMenu>
       <MenuHeader>
         <UserInfo>
+          <UserAvatar />
           <UserName>krysto</UserName>
         </UserInfo>
       </MenuHeader>
       <Divider />
 
-      <NativeButton
-        borderless={false}
-        onPress={() => {
-          navigate("Tokens");
-        }}
-      >
-        <MenuItem>
-          <Text emphasis>Tokens</Text>
-        </MenuItem>
-      </NativeButton>
-      <NativeButton
-        borderless={false}
-        onPress={() => {
-          navigate("Names");
-        }}
-      >
-        <MenuItem>
-          <Text emphasis>Names</Text>
-        </MenuItem>
-      </NativeButton>
-      <NativeButton
-        borderless={false}
-        onPress={() => {
-          navigate("Namespaces");
-        }}
-      >
-        <MenuItem>
-          <Text emphasis>Namespaces</Text>
-        </MenuItem>
-      </NativeButton>
-      <NativeButton
-        borderless={false}
-        onPress={() => {
-          navigate("Assets");
-        }}
-      >
-        <MenuItem>
-          <Text emphasis>Assets</Text>
-        </MenuItem>
-      </NativeButton>
+      <MenuItem linkTo="Tokens" icon={TokenIcon} label="Tokens" />
+      <MenuItem linkTo="Names" icon={NameIcon} label="Names" />
+      <MenuItem linkTo="Namespaces" icon={NamespaceIcon} label="Namespaces" />
+      <MenuItem linkTo="Assets" icon={AssetIcon} label="Assets" />
       <Divider />
 
-      <NativeButton borderless={false}>
-        <MenuItem>
-          <Text emphasis>Change password & PIN</Text>
-        </MenuItem>
-      </NativeButton>
-      <NativeButton borderless={false}>
-        <MenuItem>
-          <Text emphasis>Change recovery phrase</Text>
-        </MenuItem>
-      </NativeButton>
-      <NativeButton borderless={false}>
-        <MenuItem>
-          <Text emphasis>Log out</Text>
-        </MenuItem>
-      </NativeButton>
+      <MenuItem label="Change password & PIN" />
+      <MenuItem label="Change recovery phrase" />
+      <MenuItem label="Log out" />
       <Divider />
 
-      <NativeButton borderless={false}>
-        <MenuItem>
-          <Text emphasis>About Nexus Wallet</Text>
-        </MenuItem>
-      </NativeButton>
+      <MenuItem label="About Nexus Wallet" />
     </StyledSideMenu>
   );
 }
