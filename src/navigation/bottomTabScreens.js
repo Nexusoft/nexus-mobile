49b165366daca391b7ac6b780695ce2a8faf686a
Krysto <quy.p.hoang@gmail.com>
2020-06-12 15:59:01 +0700

    filter icon on Transactions screen

--- src/navigation/bottomTabScreens.js
+++ src/navigation/bottomTabScreens.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { shadow } from 'react-native-paper';
+import { shadow, IconButton } from 'react-native-paper';
 
 import { Icon } from 'components/Typo';
+import SvgIcon from 'components/SvgIcon';
+import HomeScreen from 'screens/HomeScreen';
+import TransactionsScreen from 'screens/TransactionsScreen';
 import HomeIcon from 'icons/home.svg';
 import TransactionIcon from 'icons/transaction.svg';
 import LogoIcon from 'icons/logo-full.svg';
-import HomeScreen from 'screens/HomeScreen';
-import TransactionsScreen from 'screens/TransactionsScreen';
+import AdjustIcon from 'icons/adjust.svg';
 
 export const screens = [
   {
@@ -32,6 +34,16 @@ export const screens = [
       title: 'Transactions',
       headerTitle: 'Transactions',
       headerTitleAlign: 'left',
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon={({ size }) => (
+            <SvgIcon icon={AdjustIcon} size={size} color={tintColor} />
+          )}
+          color={tintColor}
+          size={25}
+          onPress={() => {}}
+        />
+      ),
     },
   },
 ];
