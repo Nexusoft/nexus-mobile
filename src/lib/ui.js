063c6fd624db7055f3cd8d7d68a40055b14bae99
Krysto <quy.p.hoang@gmail.com>
2020-06-16 17:07:09 +0700

    dismiss keyboard when closing the filters

--- src/lib/ui.js
+++ src/lib/ui.js
@@ -1,10 +1,11 @@
-import { LayoutAnimation } from 'react-native';
+import { LayoutAnimation, Keyboard } from 'react-native';
 
 import * as TYPE from 'consts/actionTypes';
 import { getStore } from 'store';
 
 export function toggleTransactionsFilter() {
   LayoutAnimation.easeInEaseOut();
+  Keyboard.dismiss();
   getStore().dispatch({
     type: TYPE.TOGGLE_TRANSACTIONS_FILTER,
   });
