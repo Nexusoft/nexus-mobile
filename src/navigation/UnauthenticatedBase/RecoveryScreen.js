616607dacb6388abc745e27deae53f0b5d59805a
Krysto <quy.p.hoang@gmail.com>
2020-07-29 11:15:00 +0700

    added confirmation step to Recovery screen

--- src/navigation/UnauthenticatedBase/RecoveryScreen.js
+++ src/navigation/UnauthenticatedBase/RecoveryScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Formik } from 'formik';
-import { FAB } from 'react-native-paper';
+import { FAB, Dialog } from 'react-native-paper';
+import * as yup from 'yup';
 
 import Text from 'components/Text';
 import TextBox from 'components/TextBox';
 import SvgIcon from 'components/SvgIcon';
+import Portal from 'components/Portal';
 import { useTheme } from 'lib/theme';
 import { refreshUserStatus } from 'lib/user';
 import { sendAPI } from 'lib/api';
@@ -30,7 +32,54 @@ const styles = {
   },
 };
 
-function RecoveryForm({ handleSubmit, isSubmitting }) {
+function ConfirmCredentialsDialog({
+  visible,
+  onDismiss,
+  credentials,
+  onConfirm,
+}) {
+  return (
+    <Portal>
+      <Dialog visible={visible} onDismiss={onDismiss}>
+        <Formik
+          initialValues={{ password: '', pin: '' }}
+          validationSchema={yup.object().shape({
+            password: yup.string().oneOf([credentials.password], 'Mismatch!'),
+            pin: yup.string().oneOf([credentials.pin], 'Mismatch!'),
+          })}
+          onSubmit={onConfirm}
+        >
+          {({ handleSubmit }) => (
+            <Dialog.Content>
+              <TextBox.Formik
+                name="password"
+                label="Re-enter new password"
+                background={['surface', 2]}
+                secure
+                style={styles.field}
+              />
+              <TextBox.Formik
+                name="pin"
+                label="Re-enter new PIN"
+                background={['surface', 2]}
+                secure
+                style={styles.field}
+              />
+              <FAB
+                style={styles.submitBtn}
+                onPress={handleSubmit}
+                label="Confirm"
+              />
+            </Dialog.Content>
+          )}
+        </Formik>
+      </Dialog>
+    </Portal>
+  );
+}
+
+function RecoveryForm({ values, handleSubmit, isSubmitting }) {
+  const [confirming, setConfirming] = React.useState(false);
   return (
     <View>
       <TextBox.Formik
@@ -66,6 +115,17 @@ function RecoveryForm({ handleSubmit, isSubmitting }) {
         loading={isSubmitting}
         onPress={handleSubmit}
         label={isSubmitting ? 'Recovering' : 'Recover'}
+      />
+      <ConfirmCredentialsDialog
+        visible={confirming}
+        onDismiss={() => {
+          setConfirming(false);
+        }}
+        credentials={values}
+        onConfirm={() => {
+          setConfirming(false);
+          handleSubmit();
+        }}
       />
     </View>
   );
