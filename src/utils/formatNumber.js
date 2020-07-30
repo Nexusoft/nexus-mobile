6cdad42a4b443a46ef940a28bf3b38c701e6155d
Krysto <quy.p.hoang@gmail.com>
2020-07-30 12:29:48 +0700

    improved the formatNumber function

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,6 +1,3 @@
-const numberRegex = /^(\d+)\.?(\d*)$/;
-const nonZeroDigit = /[^0]/;
-
 export default function formatNumber(
   number,
   { maximumFractionDigits } = {
@@ -8,29 +5,17 @@ export default function formatNumber(
   }
 ) {
   number = Number(number);
-  const string = number.toString();
-  const match = numberRegex.exec(string);
-  if (!match) return '';
+  if (!Number.isFinite(number)) return '';
 
-  let [_, integer, fraction] = match;
-  integer = Intl.NumberFormat().format(integer);
-
-  let approximate = false;
-  if (fraction.length > maximumFractionDigits) {
-    if (integer === '0') {
-      const firstNonZeroDigitIndex = fraction.search(nonZeroDigit);
-      if (firstNonZeroDigitIndex >= maximumFractionDigits) {
-        maximumFractionDigits = firstNonZeroDigitIndex + 1;
-      }
-    }
-
-    const cutOffDigits = fraction.length - maximumFractionDigits;
-    if (cutOffDigits) {
-      const tempFraction = Number(fraction) / 10 ** cutOffDigits;
-      fraction = Math.round(tempFraction).toString();
-      approximate = true;
+  if (number) {
+    const absNumber = Math.abs(number);
+    while (absNumber < 10 ** -maximumFractionDigits) {
+      ++maximumFractionDigits;
     }
   }
 
-  return `${approximate ? '≈' : ''}${integer}${fraction ? '.' + fraction : ''}`;
+  const approximate = !Number.isInteger(number * 10 ** maximumFractionDigits);
+  return `${approximate ? '~' : ''}${Intl.NumberFormat('en', {
+    maximumFractionDigits,
+  }).format(number)}`;
 }
