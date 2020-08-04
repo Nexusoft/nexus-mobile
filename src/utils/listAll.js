8904bdb448729463791dff4762c9e0ca51f12edd
Krysto <quy.p.hoang@gmail.com>
2020-08-04 11:50:12 +0700

    listAll utility

---
+++ src/utils/listAll.js
@@ -0,0 +1,15 @@
+import { sendAPI } from 'lib/api';
+
+export default async function listAll(endpoint, params, limit = 100) {
+  let list = [];
+  let results = null;
+  let page = 0;
+  do {
+    results = await sendAPI(endpoint, { ...params, limit, page: page++ });
+    if (!results) break;
+    if (Array.isArray(results)) {
+      list = list.concat(results);
+    }
+  } while (results && results.length === limit);
+  return list;
+}
