3d1289c45d84a60be6492f211e05eccb0585e100
Krysto <quy.p.hoang@gmail.com>
2020-08-26 17:37:34 +0700

    fixed endReached condition

--- src/store/reducers/transactions/loadedAll.js
+++ src/store/reducers/transactions/loadedAll.js
@@ -5,7 +5,7 @@ const initialState = false;
 export default (state = initialState, action) => {
   switch (action.type) {
     case TYPE.LOAD_TRANSACTIONS:
-      return state && action.payload.endReached;
+      return action.payload.endReached;
 
     case TYPE.DISCONNECT_CORE:
     case TYPE.LOGOUT:
