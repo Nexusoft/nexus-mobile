32638763cc74b8cae438e2f5605b9b9dfdb6f07a
Krysto <quy.p.hoang@gmail.com>
2020-12-03 14:16:02 +0700

    hoist strings

--- src/components/CustomBottomTabBar.js
+++ src/components/CustomBottomTabBar.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Platform, Keyboard } from 'react-native';
 import { BottomTabBar } from '@react-navigation/bottom-tabs';
 
+const showEvent = Platform.select({
+  android: 'keyboardDidShow',
+  ios: 'keyboardWillShow',
+});
+const hideEvent = Platform.select({
+  android: 'keyboardDidHide',
+  ios: 'keyboardWillHide',
+});
+
 export default function CustomBottomTabBar({ style, ...rest }) {
   const [hidden, setHidden] = React.useState(false);
   React.useEffect(() => {
@@ -11,14 +20,6 @@ export default function CustomBottomTabBar({ style, ...rest }) {
     const handleKeyboardHide = () => {
       setHidden(false);
     };
-    const showEvent = Platform.select({
-      android: 'keyboardDidShow',
-      ios: 'keyboardWillShow',
-    });
-    const hideEvent = Platform.select({
-      android: 'keyboardDidHide',
-      ios: 'keyboardWillHide',
-    });
     Keyboard.addListener(showEvent, handleKeyboardShow);
     Keyboard.addListener(hideEvent, handleKeyboardHide);
     return () => {
