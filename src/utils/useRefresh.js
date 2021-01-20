a5c8ed1dd1010ece96d447b5929f97022973238f
Krysto <quy.p.hoang@gmail.com>
2021-01-20 17:53:25 +0700

    pull to refresh for Transaction Details

--- src/utils/useRefresh.js
+++ src/utils/useRefresh.js
@@ -2,14 +2,14 @@ import { useState, useCallback } from 'react';
 
 export default function useRefresh(doRefresh) {
   const [refreshing, setRefreshing] = useState(false);
-  const refresh = useCallback(async () => {
+  const refresh = async () => {
     setRefreshing(true);
     try {
       await doRefresh();
     } finally {
       setRefreshing(false);
     }
-  }, [refreshing]);
+  };
 
   return [refreshing, refresh];
 }
