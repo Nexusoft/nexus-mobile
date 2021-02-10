4f78f0245ef0895832ec18f8f198a82353416c6d
Kendal Cormany <Kendal.Cormany@nexus.io>
2021-02-10 11:02:06 -0700

    Should be =

--- src/lib/transactions.js
+++ src/lib/transactions.js
@@ -54,7 +54,7 @@ export async function refreshGenesisTx() {
       where: [
         {
           field: 'confirmations',
-          op: '<',
+          op: '=',
           value: '0',
         },
       ],
