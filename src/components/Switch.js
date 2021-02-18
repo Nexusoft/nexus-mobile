be554c97fa039359a93a60172747134c63e2ba47
Krysto <quy.p.hoang@gmail.com>
2021-02-18 13:25:29 +0700

    Formik adapter for Switch component

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { Switch as PaperSwitch } from 'react-native-paper';
+import { useField } from 'formik';
+
 import { useTheme } from 'lib/theme';
 
 export default function Switch(props) {
   const theme = useTheme();
   return <PaperSwitch color={theme.primary} {...props} />;
 }
+
+function FormikSwitch({ name, onValueChange, ...rest }) {
+  const [field, meta, helpers] = useField(name);
+  return (
+    <>
+      <Switch
+        value={field.value}
+        onValueChange={(value) => {
+          onValueChange && onValueChange(value);
+          helpers.setValue(value);
+        }}
+        {...rest}
+      />
+    </>
+  );
+}
+
+Switch.Formik = FormikSwitch;
