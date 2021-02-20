fa66092840993e9cd50c892dd5f8a4985bce666b
Krysto <quy.p.hoang@gmail.com>
2021-02-20 10:15:17 +0700

    hide unused trust account

--- src/App/OverviewScreen/Accounts.js
+++ src/App/OverviewScreen/Accounts.js
@@ -14,6 +14,7 @@ import { selectSetting } from 'lib/settings';
 import { useTheme } from 'lib/theme';
 import formatNumber from 'utils/formatNumber';
 import useRefresh from 'utils/useRefresh';
+import memoize from 'utils/memoize';
 import WalletIcon from 'icons/wallet.svg';
 
 const styles = {
@@ -67,54 +68,52 @@ const styles = {
 function Account({ account }) {
   const theme = useTheme();
   const hideBalances = useSelector(selectSetting('hideBalances'));
+
   return (
-    <>
-      <View style={styles.account}>
-        <TouchableRipple
-          onPress={() => {
-            navigate('AccountDetails', { address: account.address });
-          }}
-        >
-          <View style={styles.accInfo}>
-            <View style={styles.accNameIcon}>
-              <SvgIcon icon={WalletIcon} size={14} style={styles.accIcon} />
-              <Text style={styles.accName} bold disabled={!account.name}>
-                {account.name || 'Unnamed'}
-              </Text>
-            </View>
-            <Text style={styles.accBalance}>
-              {hideBalances
-                ? '???'
-                : formatNumber(account.balance + (account.stake || 0))}{' '}
-              <TokenName account={account} />
+    <View style={styles.account}>
+      <TouchableRipple
+        onPress={() => {
+          navigate('AccountDetails', { address: account.address });
+        }}
+      >
+        <View style={styles.accInfo}>
+          <View style={styles.accNameIcon}>
+            <SvgIcon icon={WalletIcon} size={14} style={styles.accIcon} />
+            <Text style={styles.accName} bold disabled={!account.name}>
+              {account.name || 'Unnamed'}
             </Text>
           </View>
+          <Text style={styles.accBalance}>
+            {hideBalances
+              ? '???'
+              : formatNumber(account.balance + (account.stake || 0))}{' '}
+            <TokenName account={account} />
+          </Text>
+        </View>
+      </TouchableRipple>
+
+      <View style={styles.accActions}>
+        <TouchableRipple
+          style={styles.accAction}
+          onPress={() => {
+            navigate('Receive', { account });
+          }}
+        >
+          <Text style={styles.accActionLabel({ theme })}>RECEIVE</Text>
         </TouchableRipple>
 
-        <View style={styles.accActions}>
-          <TouchableRipple
-            style={styles.accAction}
-            onPress={() => {
-              navigate('Receive', { account });
-            }}
-          >
-            <Text style={styles.accActionLabel({ theme })}>RECEIVE</Text>
-          </TouchableRipple>
+        <Divider vertical inset={10} />
 
-          <Divider vertical inset={10} />
-
-          <TouchableRipple
-            style={styles.accAction}
-            onPress={() => {
-              navigate('Send', { account });
-            }}
-          >
-            <Text style={styles.accActionLabel({ theme })}>SEND</Text>
-          </TouchableRipple>
-        </View>
+        <TouchableRipple
+          style={styles.accAction}
+          onPress={() => {
+            navigate('Send', { account });
+          }}
+        >
+          <Text style={styles.accActionLabel({ theme })}>SEND</Text>
+        </TouchableRipple>
       </View>
-      <Divider inset={20} />
-    </>
+    </View>
   );
 }
 
@@ -122,7 +121,15 @@ const refreshData = () =>
   Promise.all([refreshUserAccounts, refreshUserBalances, refreshMarketPrice]);
 
 export default function Accounts() {
+  const hideUnusedTrustAccount = useSelector(
+    selectSetting('hideUnusedTrustAccount')
+  );
   const accounts = useSelector((state) => state.user.accounts);
+  const filteredAccounts = hideUnusedTrustAccount
+    ? accounts?.filter(
+        (account) => !(account.stake === 0 && account.balance === 0)
+      )
+    : accounts;
   const [refreshing, refresh] = useRefresh(refreshData);
   return (
     <>
@@ -139,9 +146,10 @@ export default function Accounts() {
         refreshing={refreshing}
         onRefresh={refresh}
         style={styles.accounts}
-        data={accounts}
+        data={filteredAccounts}
         keyExtractor={(acc) => acc.address}
         renderItem={({ item: account }) => <Account account={account} />}
+        ItemSeparatorComponent={() => <Divider inset={20} />}
       />
     </>
   );
