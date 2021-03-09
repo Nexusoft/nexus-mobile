66522791392ca8be50ff2080e992f6ee83afa1db
Kendal Cormany <Kendal.Cormany@nexus.io>
2021-03-09 15:31:24 -0700

    Wrapped read logfile with try catch

--- src/App/screens/EmeddedCoreLogReaderScreen.js
+++ src/App/screens/EmeddedCoreLogReaderScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TextInput, Platform } from 'react-native';
 import { Surface, Button, IconButton } from 'react-native-paper';
 
@@ -9,6 +9,7 @@ import { useTheme } from 'lib/theme';
 import RNFS from 'react-native-fs';
 import SvgIcon from 'components/SvgIcon';
 import RecoveryIcon from 'icons/recovery.svg';
+import { showError } from 'lib/ui';
 
 const styles = {
   screen: {
@@ -16,55 +17,63 @@ const styles = {
   },
 };
 
-
-async function handleReadFile (logOutput,setLogOutput)
-{
-    if (logOutput !== true || logOutput === 'finished')
-        return;
-    if (logOutput === true)
-    {
-        //TODO: Change this
-        const fileStats = await RNFS.stat((Platform.OS === 'android' ? RNFS.ExternalDirectoryPath : RNFS.DocumentDirectoryPath) + '/Nexus/client/log/0.log');
-        const bytestoread = 50000;
-        const byteposition = Math.sign(fileStats.size - bytestoread) ? fileStats.size - bytestoread :fileStats.size ;
-        const result = await RNFS.read((Platform.OS === 'android' ? RNFS.ExternalDirectoryPath : RNFS.DocumentDirectoryPath) + "/Nexus/client/log/0.log",50000,byteposition,'ascii');
-        const formated = result.split(`\n`).reverse().join(`\n`);
-        setLogOutput(formated);
+async function handleReadFile(logOutput, setLogOutput) {
+  if (logOutput !== true || logOutput === 'finished') return;
+  if (logOutput === true) {
+    //TODO: Change this
+    try {
+      const fileStats = await RNFS.stat(
+        (Platform.OS === 'android'
+          ? RNFS.ExternalDirectoryPath
+          : RNFS.DocumentDirectoryPath) + '/Nexus/client/log/0.log'
+      );
+      const bytestoread = 50000;
+      const byteposition = Math.sign(fileStats.size - bytestoread)
+        ? fileStats.size - bytestoread
+        : fileStats.size;
+      const result = await RNFS.read(
+        (Platform.OS === 'android'
+          ? RNFS.ExternalDirectoryPath
+          : RNFS.DocumentDirectoryPath) + '/Nexus/client/log/0.log',
+        50000,
+        byteposition,
+        'ascii'
+      );
+      const formated = result.split(`\n`).reverse().join(`\n`);
+      setLogOutput(formated);
+    } catch (error) {
+      showError(error?.message);
     }
+  }
 }
-
-
 
 const LogOutput = () => {
-    const {logOutput, setLogOutput} = EmbeddedCoreLogReaderScreen.logCallback;
-    handleReadFile(logOutput, setLogOutput);
-    const theme = useTheme();
-    if (logOutput === true)
-    {
-        return (<Text style={{color: theme.foreground}}>Loading</Text>);
-    }
-    else{
+  const { logOutput, setLogOutput } = EmbeddedCoreLogReaderScreen.logCallback;
+  handleReadFile(logOutput, setLogOutput);
+  const theme = useTheme();
+  if (logOutput === true) {
+    return <Text style={{ color: theme.foreground }}>Loading</Text>;
+  } else {
     return (
-        <TextInput multiline editable={false} style={{color: theme.foreground}}>
-            {logOutput}
-        </TextInput>
-    )}
-}
-
+      <TextInput multiline editable={false} style={{ color: theme.foreground }}>
+        {logOutput}
+      </TextInput>
+    );
+  }
+};
 
 const UpdateLog = () => {
-    EmbeddedCoreLogReaderScreen.logCallback.setLogOutput(true);
-}
+  EmbeddedCoreLogReaderScreen.logCallback.setLogOutput(true);
+};
 
-
-export default function EmbeddedCoreLogReaderScreen( { navigation }) {
-    const [logOutput, setLogOutput] = useState(true);
-    EmbeddedCoreLogReaderScreen.logCallback = {logOutput, setLogOutput};
+export default function EmbeddedCoreLogReaderScreen({ navigation }) {
+  const [logOutput, setLogOutput] = useState(true);
+  EmbeddedCoreLogReaderScreen.logCallback = { logOutput, setLogOutput };
   return (
     <ScreenBody style={styles.screen}>
-        <ScrollView style={{margin:10, flex:1}}>
-            <LogOutput />
-        </ScrollView>   
+      <ScrollView style={{ margin: 10, flex: 1 }}>
+        <LogOutput />
+      </ScrollView>
     </ScreenBody>
   );
 }
@@ -76,16 +85,17 @@ EmbeddedCoreLogReaderScreen.nav = {
   name: 'EmbeddedCoreLogReader',
   options: {
     title: 'Core Log',
-    headerRight: ({tintColor}) =>     
-        <IconButton
-            icon={({ size }) => (
-                <SvgIcon icon={RecoveryIcon} size={size} color={tintColor} />
-            )}
-            color={tintColor}
-            size={25}
-            onPress={() => {
-                UpdateLog();
-            }}
-        />
+    headerRight: ({ tintColor }) => (
+      <IconButton
+        icon={({ size }) => (
+          <SvgIcon icon={RecoveryIcon} size={size} color={tintColor} />
+        )}
+        color={tintColor}
+        size={25}
+        onPress={() => {
+          UpdateLog();
+        }}
+      />
+    ),
   },
 };
