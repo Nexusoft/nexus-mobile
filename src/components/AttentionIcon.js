f2d27172bcf0d1dc68b2dbe368f07e8d85599336
Krysto <quy.p.hoang@gmail.com>
2021-03-15 14:10:37 +0700

    refactoring

--- src/components/AttentionIcon.js
+++ src/components/AttentionIcon.js
@@ -4,22 +4,22 @@ import SvgIcon from 'components/SvgIcon';
 import { useTheme } from 'lib/theme';
 import WarningIcon from 'icons/warning-circle.svg';
 
-const defaultStyle = ({ theme, size }) => ({
-  position: 'absolute',
-  backgroundColor: theme.onDanger,
-  overflow: 'hidden',
-  borderRadius: size / 2,
-});
-
-export default function AttentionIcon({ style, ...rest }) {
+export default function AttentionIcon({ style, size = 12, ...rest }) {
   const theme = useTheme();
-  const size = rest.size || 12;
   return (
     <SvgIcon
       icon={WarningIcon}
-      style={{ ...defaultStyle({ theme, size }), ...style }}
+      style={[
+        {
+          backgroundColor: theme.onDanger,
+          overflow: 'hidden',
+          borderRadius: size / 2,
+        },
+        style,
+      ]}
       size={size}
       color={theme.danger}
+      {...rest}
     />
   );
 }
