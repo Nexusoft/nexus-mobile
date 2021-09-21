6df1cc1af89456bfe13dcfdba34ac42d3208c89b
Kendal Cormany <Kendal.Cormany@nexus.io>
2021-09-20 18:10:10 -0700

    5.0.5 iOS core

--- ios/nexus-mobile-library.framework/Headers/LLP/templates/ddos.h
+++ ios/nexus-mobile-library.framework/Headers/LLP/templates/ddos.h
@@ -2,7 +2,7 @@
 
             (c) Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
 
-            (c) Copyright The Nexus Developers 2014 - 2019
+            (c) Copyright The Nexus Developers 2014 - 2021
 
             Distributed under the MIT software license, see the accompanying
             file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -83,6 +83,10 @@ namespace LLP
          *
          **/
         DDOS_Score(int nTimespan);
+
+
+        /** Default Destructor. **/
+        ~DDOS_Score();
 
 
         /** Flush
