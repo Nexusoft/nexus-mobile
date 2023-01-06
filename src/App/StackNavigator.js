1002550048350b74fed56f63b91c5109b5140f48
Krysto <quy.p.hoang@gmail.com>
2023-01-06 09:43:48 +0700

    HeaderBackground is now imported from @react-navigation/elements

--- src/App/StackNavigator.js
+++ src/App/StackNavigator.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Platform, KeyboardAvoidingView } from 'react-native';
-import {
-  createStackNavigator,
-  Header,
-  HeaderBackground,
-} from '@react-navigation/stack';
+import { createStackNavigator, Header } from '@react-navigation/stack';
+import { HeaderBackground } from '@react-navigation/elements';
 import { shadow } from 'react-native-paper';
 
 import ReceiveScreen from './screens/ReceiveScreen';
